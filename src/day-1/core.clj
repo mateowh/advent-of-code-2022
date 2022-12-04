714a2e13897e6ca677186ba51581d9709bc6055e
Matthew Whitaker <matthew.whitaker@signal-ai.com>
2022-12-04 21:20:56 +0000

    Added part 2 of day 1

--- src/day-1/core.clj
+++ src/day-1/core.clj
@@ -3,8 +3,16 @@
   (let [split (clojure.string/split group #"\n")]
     (map #(Integer/parseInt %) split)))
 
-(defn find-max-cals []
-  (let [input (slurp "input.txt")
-        food-per-elf (clojure.string/split input #"\n\n")
+(def input-data
+  (let [raw-input (slurp "src/day-1/input.txt")
+        food-per-elf (clojure.string/split raw-input #"\n\n")
         total-cals-per-elf (map #(->> % cal-string->cals (apply +)) food-per-elf)]
-    (apply max total-cals-per-elf)))
+    total-cals-per-elf))
+
+;; part 1
+(defn find-max-cals []
+  (apply max input-data))
+
+;; part 2
+(defn sum-top-3-elf-cals []
+  (->> input-data (sort >) (take 3) (apply +)))
